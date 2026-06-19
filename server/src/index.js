a0ed685377453f055ed62c9a6c8c69a64d212915
SNP-0309 <srujanpoojari2@gmail.com>
2026-06-19 18:42:36 +0530

    Succesfully Integraed gemini ai model in this project

--- server/src/index.js
+++ server/src/index.js
@@ -1,13 +1,11 @@
 import app from './app.js';
 import dotenv from 'dotenv';
 import connectDB from './config/db.js';
-import invokeGeminai from './services/ai.service.js';
+
 dotenv.config();
 connectDB();
 
-const response = await invokeGeminai(
-  "Hello Gemini! Explain What is interview ?."
-);
+
 
 const PORT = process.env.PORT || 5000;
 console.log(response)
--- server/src/services/ai.service.js
+++ server/src/services/ai.service.js
@@ -1,21 +1,22 @@
 import { GoogleGenAI } from "@google/genai";
 import { configDotenv } from "dotenv";
+import {z} from "zod"
+import { zodToJsonSchema } from "zod-to-json-schema";
 const ai = new GoogleGenAI(
     {
         apiKey: process.env.GOOGLE_GENAI_API_KEY
     }
 )
-const invokeGeminai = async (prompt) => {
-  try {
-    const result = await ai.models.generateContent({
-      model: "gemini-2.5-flash",
-      contents: prompt,
-    });
+const interviewReportSchema = z.object({
+  technicalQuestion : z.array(z.object({
+    question: z.string().description("Technical question would be asked in the interview."),
+    intension: z.string().description("Intension of asking this type of questions on the interview."),
+    answer:z.string().description("The answers of the given technical question asked in the interview.")
 
-    return result.text;
-  } catch (error) {
-    console.error("Gemini Error:", error);
-    throw new Error("Failed to generate AI response");
-  }
-};
-export default invokeGeminai
+  }))
+})
+
+async function GenarateInterviewReport({resume,selfdescription,jobdescription}) {
+
+  
+}
