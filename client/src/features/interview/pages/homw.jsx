0d3afa9769e2499a5ec30d45b8c8ae4474d779af
SNP-0309 <srujanpoojari2@gmail.com>
2026-06-19 23:09:21 +0530

    Interview backend completed

---
+++ client/src/features/interview/pages/homw.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const homw = () => {
+  return (
+    <main className='home'>
+<div className='left'><textarea name="JobDiscription" placeholder='Enter Job description here ...' id="jobDescription"></textarea></div>
+    </main>
+  )
+}
+
+export default homw
--- server/src/middlewares/auth.middleware.js
+++ server/src/middlewares/auth.middleware.js
@@ -1,43 +1,39 @@
-import jwt from "jsonwebtoken"
-import TokenBlacklist from "../models/tokenBlacklist.model.js"
-
+import jwt from "jsonwebtoken";
+import TokenBlacklist from "../models/tokenBlacklist.model.js";
 
 async function authUser(req, res, next) {
+  try {
+    const authHeader = req.headers.authorization;
 
-    const token = req.cookies.token
+    const token =
+      req.cookies?.token ||
+      (authHeader?.startsWith("Bearer ")
+        ? authHeader.split(" ")[1]
+        : null);
 
     if (!token) {
-        return res.status(401).json({
-            message: "Token not provided."
-        })
+      return res.status(401).json({
+        message: "Token not provided.",
+      });
     }
 
-    // Check if token is blacklisted
-    const isTokenBlacklisted = await TokenBlacklist.findOne({
-        token
-    })
+    const isTokenBlacklisted = await TokenBlacklist.findOne({ token });
 
     if (isTokenBlacklisted) {
-        return res.status(401).json({
-            message: "token is invalid (blacklisted)"
-        })
+      return res.status(401).json({
+        message: "Token is blacklisted.",
+      });
     }
 
-    try {
-        const decoded = jwt.verify(token, process.env.JWT_SECRET)
+    const decoded = jwt.verify(token, process.env.JWT_SECRET);
 
-        req.user = decoded
-
-        next()
-
-    } catch (err) {
-
-        return res.status(401).json({
-            message: "Invalid token."
-        })
-    }
-
+    req.user = decoded;
+    next();
+  } catch (err) {
+    return res.status(401).json({
+      message: "Invalid token.",
+    });
+  }
 }
 
-
-export { authUser }
+export { authUser };
